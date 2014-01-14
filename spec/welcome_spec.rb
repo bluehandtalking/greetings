8b95bca1a53077b07dc0dc986bb0cf02c0b03fba
Jerrold Thompson <jet@whidbey.com>
2014-01-14 12:49:09 -0800

    Welcome 2

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "Welcome" do
    it "has a message" do
-     hello = Welcome.new
-     hello.message.must_include "Welcome"
+     @hello = Welcome.new
+     @hello.message.must_include "Welcome"
    end
 end
 
