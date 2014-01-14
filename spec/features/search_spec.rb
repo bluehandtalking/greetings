ee34c1f6459fedd33f4c42a9b87e17a64b86d72f
Jerrold Thompson <jet@whidbey.com>
2014-01-14 10:32:35 -0800

    Fix search spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe "My search page" do
     it "has results" do
        visit "http://google.com"
-       fill_in "q" with "Code Fellows"
+       fill_in "q",  with: "Code Fellows"
        click_on "Google Search"
        page.text.must_include "codefellows.org"
     end
