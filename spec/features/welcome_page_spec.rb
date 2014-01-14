d27a4723b1911e51194f9c681547ca1a10a23bb0
Jerrold Thompson <jet@whidbey.com>
2014-01-14 13:06:10 -0800

    welcome 3

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
+require '../welcome'
 
 describe "My welcome page" do
     it  "has a Welcome message" do
        visit "file://localhost/home/jet/Code_Fellows/welcome_project/public/index.html"
-       page.text.must_include "Welcome Aboard"
+       page.text.must_include "Welcome"
     end
 end
--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,9 +1,11 @@
-require 'spec_helper'
+require "minitest/spec"
+require "minitest/autorun"
+require "welcome" # Adding class from lib
 
 describe "Welcome" do
    it "has a message" do
-     @hello = Welcome.new
-     @hello.message.must_include "Welcome"
+     hello = Welcome.new
+     hello.message.must_include  "Welcome"
    end
 end
 
